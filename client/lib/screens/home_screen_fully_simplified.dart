0fe6cbdf7ea6db3add308a14b4d0851397a82ab2
James <isd6542@outlook.com>
2025-06-25 21:55:38 +1000

    Autofix

--- client/lib/screens/home_screen_fully_simplified.dart
+++ client/lib/screens/home_screen_fully_simplified.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:just_audio/just_audio.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
