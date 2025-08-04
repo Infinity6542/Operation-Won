ca01433acbbb1a3e5b9a7acbfde124d04ba34113
James <isd6542@outlook.com>
2025-08-04 23:46:46 +1000

    .

--- server/update_mysql_permissions.sql
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# update_mysql_permissions.sql
-# This SQL script is for emergency use if MySQL permissions are out of sync
-# Run it with: docker exec -i opwon_mysql mysql -uroot -p < update_mysql_permissions.sql
-
-CREATE USER IF NOT EXISTS 'opwon_user'@'%' IDENTIFIED BY 'opwon_password';
-GRANT ALL PRIVILEGES ON operation_won.* TO 'opwon_user'@'%';
-FLUSH PRIVILEGES;
