b675de7f6af003bcd2ae3b8b99abe3c8ba68c110
James <isd6542@outlook.com>
2025-09-24 23:25:41 +1000

    Plan encryption

---
+++ client/lib/services/encryption_service.dart
@@ -0,0 +1,9 @@
+//TODO: Make and implement encryption service
+// Key management
+// Key exchange and setup --> websocket service
+// Key rotation
+// Session management
+// Chunk en/decryption --> audio service
+// 
+// Also maybe move away from opus
+// Consider lossless
